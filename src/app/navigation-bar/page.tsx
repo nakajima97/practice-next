99c75849e8b1edbf822d37ac001e3e1261f683e3
nakajima97 <kituneudon97@gmail.com>
2024-08-13 18:11:37 +0900

    #11 サンプルとするコードのHTML要素を修正

--- src/app/navigation-bar/page.tsx
+++ src/app/navigation-bar/page.tsx
@@ -1,74 +1,91 @@
-'use client';
-
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
+import { Box, IconButton, Typography } from '@mui/material';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import { autocompleteClasses } from '@mui/material';
+import AccessibilityIcon from '@mui/icons-material/Accessibility';
+import AddToQueueIcon from '@mui/icons-material/AddToQueue';
 
 const NavigationBarPage = () => {
 	return (
 		<Box
 			sx={{
-				width: {xs: '100dvh', sm: 250},
+				width: { xs: '100dvh', sm: 250 },
 				backgroundColor: 'lightgrey',
-				height: {xs: '60px', sm: '100dvh'},
+				height: { xs: '600px', sm: '100dvh' },
 				display: 'flex',
+				flexDirection: { xs: 'row', sm: 'column' },
 				position: 'fixed',
-				top: {xs: 'auto', sm: 0},
+				top: { xs: 'auto', sm: 0 },
 				left: 0,
-				bottom: {xs: 0, sm: 'auto'},
-				flexDirection: 'column',
+				bottom: { xs: 0, sm: 'auto' },
 				justifyContent: 'space-between',
 			}}
 			role="presentation"
 		>
-			<List>
-        <ListItem disablePadding sx={{ display: {xs: 'none', md: 'block'} }}>
-						<ListItemButton>
-							<ListItemIcon>
-								<InboxIcon />
-							</ListItemIcon>
-							<ListItemText primary='PC Only' />
-						</ListItemButton>
-					</ListItem>
-          <ListItem disablePadding>
-						<ListItemButton>
-							<ListItemIcon>
-								<MailIcon />
-							</ListItemIcon>
-							<ListItemText primary='Starred' />
-						</ListItemButton>
-					</ListItem>
-          <ListItem disablePadding>
-						<ListItemButton>
-							<ListItemIcon>
-								<AccessAlarmIcon />
-							</ListItemIcon>
-							<ListItemText primary='Send email' />
-						</ListItemButton>
-					</ListItem>
-			</List>
-			<Divider />
-			<List>
-				{['All mail', 'Trash'].map((text, index) => (
-					<ListItem key={text} disablePadding>
-						<ListItemButton>
-							<ListItemIcon>
-								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-							</ListItemIcon>
-							<ListItemText primary={text} />
-						</ListItemButton>
-					</ListItem>
-				))}
-			</List>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: { xs: 'row', sm: 'column' },
+					height: { xs: '60px', sm: '100%' },
+					width: '200px'
+				}}
+			>
+				<Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+					<IconButton>
+						<InboxIcon />
+					</IconButton>
+					<Typography
+						variant="body2"
+						sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
+					>
+						PC Only
+					</Typography>
+				</Box>
+				<Box sx={{ display: 'flex' }}>
+					<IconButton>
+						<MailIcon />
+					</IconButton>
+					<Typography
+						variant="body2"
+						sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
+					>
+						Starred
+					</Typography>
+				</Box>
+				<Box sx={{ display: 'flex' }}>
+					<IconButton>
+						<AccessAlarmIcon />
+					</IconButton>
+					<Typography
+						variant="body2"
+						sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
+					>
+						Send email
+					</Typography>
+				</Box>
+				<Box sx={{ display: 'flex' }}>
+					<IconButton>
+						<AccessibilityIcon />
+					</IconButton>
+					<Typography
+						variant="body2"
+						sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
+					>
+						Starred
+					</Typography>
+				</Box>
+				<Box sx={{ display: 'flex' }}>
+					<IconButton>
+						<AddToQueueIcon />
+					</IconButton>
+					<Typography
+						variant="body2"
+						sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}
+					>
+						Send email
+					</Typography>
+				</Box>
+			</Box>
 		</Box>
 	);
 };
